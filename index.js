e47e40d104b4c6c388ac1ce4436b13434a2605fb
kaiosilva17 <129460757+kaiosilva17@users.noreply.github.com>
2024-03-06 22:09:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', function(req,res){
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.get('/hello', function (req, res) {
     res.send("Chegou a resposta")
 })
 
-app.get('/nome', function(req,res){
+app.get('/nome', function (req, res) {
     res.send("Kaio")
 })
 
+app.post('/lista1/ex2', function (req, res) {
 
-app.listen('3000', function(){
+    const total = Number(req.body.total)
+    const brancos = Number(req.body.brancos)
+    const nulos = Number(req.body.nulos)
+    const validos = Number(req.body.validos)
+
+
+    const soma = brancos + nulos + validos
+
+    let retorno = {}
+    if (soma > total) {
+
+        retorno = {
+            codigo: 'SOMA_ELEITORES',
+            mensagem: "A soma dos votos não pode passar o total de eleitores."
+        }
+
+        res.status(400).json(retorno)
+
+    } else {
+
+        const perBrancos = brancos / total * 100 + '%'
+        const perNulos = nulos / total * 100 + '%'
+        const perValidos = validos / total * 100 + '%'
+
+        retorno = { perBrancos, perNulos, perValidos }
+
+        res.status(200).json(retorno)
+
+
+    }
+
+
+})
+
+
+app.listen('3000', function () {
 
 })
